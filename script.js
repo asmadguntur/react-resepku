8b815e44c50b7a0d47dc4c8fd51fb623ce36c90d
asmadguntur <asmadgutnur@gmail.com>
2026-05-07 10:19:30 +0800

    Remove CSS file usage, use inline style for filled icon

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ favBtns.forEach(favBtn => {
   const loveIcon = favBtn.querySelector('span')
   favBtn.addEventListener('click', function() {
     loveIcon.classList.toggle('text-red-500')
-    loveIcon.classList.toggle('filled')
+    if (loveIcon.style.fontVariationSettings === "'FILL' 1") {
+      loveIcon.style.fontVariationSettings = ""
+    } else {
+      loveIcon.style.fontVariationSettings = "'FILL' 1"
+    }
   })
 })
